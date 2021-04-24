cb2d409b1b15a37336cac24b286149b72527c923
SHIVA SANKARI C <shivasankari3101@gmail.com>
2021-04-24 22:34:18 +0530

    Some changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function navbar(){
+function open_menu(){
     document.getElementsByClassName("bars")[0].classList.toggle("cross");
     document.getElementsByClassName("menu")[0].classList.toggle("open");
     document.getElementsByClassName("navbar")[0].classList.toggle("fixed");
