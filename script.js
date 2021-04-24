a9ddae02a321859f6ef7a33a6b60acd8cf282e0b
SHIVA SANKARI C <shivasankari3101@gmail.com>
2021-04-24 21:45:44 +0530

    Almost completed

---
+++ script.js
@@ -0,0 +1,5 @@
+function navbar(){
+    document.getElementsByClassName("bars")[0].classList.toggle("cross");
+    document.getElementsByClassName("menu")[0].classList.toggle("open");
+    document.getElementsByClassName("navbar")[0].classList.toggle("fixed");
+}
