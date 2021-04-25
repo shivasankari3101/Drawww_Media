d6e38c0a66d92b553d8e9febebbb4f8ddd6f45d0
SHIVA SANKARI C <shivasankari3101@gmail.com>
2021-04-25 12:31:58 +0530

    Completed the landing page

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+// Loader function
 window.onload = function(){
-    
     var x = setInterval(function(){
         document.getElementsByClassName("loader")[0].classList.toggle("close_loader");
         clearInterval(x);
     },2000)
 }
 
+//theme toggle for about section
 document.body.addEventListener("click",function(){
       document.getElementsByClassName("about")[0].classList.toggle("about_dark");
 })
 
+//Opening menu
 function open_menu(){
     document.getElementsByClassName("bars")[0].classList.toggle("cross");
     document.getElementsByClassName("menu")[0].classList.toggle("open");
