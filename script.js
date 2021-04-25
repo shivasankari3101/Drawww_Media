baacf4577e9833f6e479f1c291f45bf51954f1c5
SHIVA SANKARI C <shivasankari3101@gmail.com>
2021-04-25 11:01:27 +0530

    Added loader

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.onload = function(){
+    
+    var x = setInterval(function(){
+        document.getElementsByClassName("loader")[0].classList.toggle("close_loader");
+        clearInterval(x);
+    },2000)
+}
+
 function open_menu(){
     document.getElementsByClassName("bars")[0].classList.toggle("cross");
     document.getElementsByClassName("menu")[0].classList.toggle("open");
